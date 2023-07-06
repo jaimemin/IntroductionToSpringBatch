cd04200fee2d4cc00298973f93ed83b532cc30b4
jaimemin <jaimemin@gmail.com>
2023-07-06 16:53:51 +0900

    JobBuilderFactory 실습

--- src/main/java/com/tistory/jaimemin/springbatch/JobLauncherConfiguration.java
+++ src/main/java/com/tistory/jaimemin/springbatch/JobLauncherConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -23,9 +25,18 @@ public class JobLauncherConfiguration {
 
     @Bean
     public Job batchJob() {
-        return this.jobBuilderFactory.get("job")
+        return this.jobBuilderFactory.get("batchJob1")
                 .start(step())
                 .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Job batchJob2() {
+        return this.jobBuilderFactory.get("batchJob2")
+                .start(flow())
+                .next(step5())
+                .end()
                 .build();
     }
 
@@ -35,7 +46,7 @@ public class JobLauncherConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        Thread.sleep(3000);
+                        System.out.println("step1 has executed");
 
                         return RepeatStatus.FINISHED;
                     }
@@ -46,7 +57,57 @@ public class JobLauncherConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((stepContribution, chunkContext) -> null)
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step3())
+                .next(step4())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step3 has executed");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step4 has executed");
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step5 has executed");
+
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 }
--- src/main/java/com/tistory/jaimemin/springbatch/Member.java
+++
@@ -1,9 +0,0 @@
-package com.tistory.jaimemin.springbatch;
-
-import lombok.Data;
-
-@Data
-public class Member {
-
-    private String id;
-}
