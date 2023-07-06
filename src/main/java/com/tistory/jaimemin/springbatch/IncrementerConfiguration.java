a20cab93dee2dd476248ab50d0c998aaae05b1f1
jaimemin <jaimemin@gmail.com>
2023-07-06 18:09:16 +0900

    SimpleJob Incrementer 예제

--- src/main/java/com/tistory/jaimemin/springbatch/IncrementerConfiguration.java
+++ src/main/java/com/tistory/jaimemin/springbatch/IncrementerConfiguration.java
@@ -1,5 +1,6 @@
 package com.tistory.jaimemin.springbatch;
 
+import com.tistory.jaimemin.CustomJobParametersIncrementer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -7,6 +8,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -15,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class IncrementerConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
 
@@ -26,9 +28,8 @@ public class ValidatorConfiguration {
         return this.jobBuilderFactory.get("batchJob")
                 .start(step())
                 .next(step2())
-                .next(step3())
-//                .validator(new CustomJobParametersValidator())
-                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
+                .incrementer(new CustomJobParametersIncrementer()) // Job을 동일한 JobParameters로 여러번 돌리고자 할 때
+                // .incrementer(new RunIdIncrementer()) // Spring에서 제공하는 기본 Incrementer
                 .build();
     }
 
@@ -57,14 +58,4 @@ public class ValidatorConfiguration {
                 .build();
     }
 
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
 }
