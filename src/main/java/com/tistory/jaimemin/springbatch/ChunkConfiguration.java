8bb6f0ae3bbffaf0c61a2f3f639e9777bdee7558
jaimemin <jaimemin@gmail.com>
2023-07-11 18:28:34 +0900

    Chunk 개념 파악

---
+++ src/main/java/com/tistory/jaimemin/springbatch/ChunkConfiguration.java
@@ -0,0 +1,59 @@
+package com.tistory.jaimemin.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class ChunkConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                .start(step())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("step")
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor((ItemProcessor<String, String>) item -> {
+                    Thread.sleep(3000);
+                    System.out.println("original item = " + item);
+
+                    return "my" + item;
+                })
+                .writer(items -> {
+                    Thread.sleep(3000);
+
+                    System.out.println("items = " + items);
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+}
